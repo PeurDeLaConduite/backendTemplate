d72554aa26984d68ed4c9ad437cef3ed27450929
Jerem16 <jeremy.lemaignent@gmail.com>
2025-05-13 12:19:41 +0200

    feature Base 5

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,33 +1,39 @@
 import { defineAuth } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
     loginWith: {
         email: true,
     },
     userAttributes: {
-        // Prénom (given_name)
         givenName: {
             mutable: true,
             required: true,
         },
-        // Nom de famille (family_name)
         familyName: {
             mutable: true,
             required: true,
         },
-        // Adresse
         address: {
             mutable: true,
             required: false,
         },
-        // Numéro de téléphone
         phoneNumber: {
             mutable: true,
             required: false,
+        },
+        "custom:display_name": {
+            dataType: "String",
+            mutable: true,
+            maxLen: 16,
+            minLen: 1,
+        },
+        "custom:is_beta_user": {
+            dataType: "Boolean",
+            mutable: true,
+        },
+        "custom:started_free_trial": {
+            dataType: "String", // ✔ Remplacé DateTime
+            mutable: true,
         },
     },
 });
