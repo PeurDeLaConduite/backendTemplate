c3d3941a20c7aed3fe297de6fb79d163ea143eb0
Jerem16 <jeremy.lemaignent@gmail.com>
2025-05-14 17:25:19 +0200

    feature Base 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,18 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import "./../app/app.css";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-    function deleteTodo(id: string) {
-        client.models.Todo.delete({ id });
-    }
-    function listTodos() {
-        client.models.Todo.observeQuery().subscribe({
-            next: (data) => setTodos([...data.items]),
-        });
-    }
-
-    useEffect(() => {
-        listTodos();
-    }, []);
-
-    function createTodo() {
-        client.models.Todo.create({
-            content: window.prompt("Todo content"),
-        });
-    }
-
     return (
         <main>
-            <h1>My todos</h1>
-            <button onClick={createTodo}>+ new</button>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-                        {todo.content}
-                    </li>
-                ))}
-            </ul>
+            <h1>Bienvenue sur l'application</h1>
             <div>
-                🥳 App successfully hosted. Try creating a new todo.
+                🥳 App successfully hosted.
                 <br />
                 <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
                     Review next steps of this tutorial.
